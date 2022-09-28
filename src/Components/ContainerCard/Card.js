681d90945f8024492db5ff9928445cef53b8d2ae
Nahid-07 <nahidasif1998@gmail.com>
2022-09-28 23:18:11 +0600

    card added

---
+++ src/Components/ContainerCard/Card.js
@@ -0,0 +1,23 @@
+const Card = ({items}) => {
+    
+    const {name,time_req,img,description,age} = items;
+    return (
+        <div className="border shadow-lg rounded mx-5">
+            <div className='p-2'>
+            <img className='rounded w-96 h-96' src={img} alt="" />
+            </div>
+            <div className="px-2 flex flex-col items-center">
+                <h1 className="text-2xl font-semibold my-4">{name}</h1>
+                <p className="my-4 text-center">{description.slice(0,100)}</p>
+                <h2>Time required : {time_req}</h2>
+                <h3 className="text-lg font-medium">Age : {age}</h3>
+                <div>
+                <button className="w-60 bg-teal-700 py-3 my-4 rounded-lg text-white font-medium text-lg">Add</button>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Card;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../ContainerCard/Card';
 import Nav from '../Nav/Nav';
 
 const Main = () => {
+    const [cards,setCards]=useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setCards(data))
+    },[])
     return (
         <div className='grid grid-cols-12'>
             <div className="main-container col-span-8">
@@ -9,8 +16,13 @@ const Main = () => {
                 <header>
                     <h1 className='text-6xl text-center my-10'><span className='text-orange-600 text-5xl font-semibold'>Train_</span><span className='text-amber-600 font-bold'>ToFit</span></h1>
                 </header>
+                <div className='grid grid-cols-3 gap-5 my-14'>
+                {
+                    cards.map(card => <Card items={card}></Card>)
+                }
+                </div>
             </div>
-            <div className="details-container col-span-4 bg-slate-200 h-screen">
+            <div className="details-container col-span-4 bg-slate-200">
                 hello cart
             </div>
         </div>
